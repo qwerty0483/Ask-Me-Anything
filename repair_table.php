4d53fecb7dab56b413a403a21feb6fc2880fda9b
정수빈 <jungsubin83@naver.com>
2024-06-15 17:38:44 +0900

    ask me anything

--- repair_table.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db = mysqli_connect('localhost', 'root', '', 'board') or die('Unable to connect. Check your connection parameters.');
-
-$query = 'REPAIR TABLE question_list';
-mysqli_query($db, $query) or die(mysqli_error($db));
-
-echo 'Table repaired successfully.';
